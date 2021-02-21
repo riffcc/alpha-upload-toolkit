46334fb6499bc380aa852433d4ed312fcd6733d2
Benjamin Arntzen <zorlin@gmail.com>
2021-02-21 18:58:55 +0800

    Add some very half baked code

---
+++ fetcher/apiup-auto.py
@@ -0,0 +1,59 @@
+import requests
+import logging
+import sys
+import os
+
+#print(os.getcwd())
+
+# Enabling debugging at http.client level (requests->urllib3->http.client)
+# you will see the REQUEST, including HEADERS and DATA, and RESPONSE with HEADERS but without DATA.
+# the only thing missing will be the response.body which is not logged.
+try: # for Python 3
+    from http.client import HTTPConnection
+except ImportError:
+    from httplib import HTTPConnection
+HTTPConnection.debuglevel = 1
+
+logging.basicConfig() # you need to initialize logging, otherwise you will not see anything from requests
+logging.getLogger().setLevel(logging.DEBUG)
+requests_log = logging.getLogger("urllib3")
+requests_log.setLevel(logging.DEBUG)
+requests_log.propagate = True
+
+headers = {'User-Agent': 'rcc-ut/1.0.1'}
+
+test_file = open(str(sys.argv[1]), "rb")
+
+from pathlib import Path
+apitoken = Path('/home/wings/.rcc-api').read_text()
+torrentdescription = Path('description.tmp').read_text()
+torrentname = str(sys.argv[3]).strip('"')
+
+payload = {
+    'nfo':'',
+    'name':str(sys.argv[3]),
+    'description':'Testing submission via Python. \n\n Source: '+str(sys.argv[2])+'\n\n'+torrentdescription,
+    'mediainfo':'',
+    'category_id':'10',
+    'type_id':'21',
+    'resolution_id':'13',
+    'user_id':'3',
+    'tmdb':'0',
+    'imdb':'0',
+    'tvdb':'0',
+    'mal':'0',
+    'igdb':'0',
+    'anonymous':'0',
+    'stream':'0',
+    'sd':'0',
+    'internal':'0',
+    'featured':'0',
+    'free':'0',
+    'doubleup':'0',
+    'sticky':'0'
+}
+
+session = requests.Session()
+r = session.post('https://u.riff.cc/api/torrents/upload?api_token='+apitoken,headers=headers,files = {'torrent': test_file},data=payload)
+
+print(r.text)
--- fetcher/librivox-auto.sh
+++ fetcher/librivox-auto.sh
@@ -31,7 +31,8 @@ for (( i = $1; i <= $2; ++i ));
 do
 	echo "Grabbing a list of torrent URLs."
         # Grab a list of torrent URLs from our source.
-        torrent=$(wget -q -O- "https://archive.org/advancedsearch.php?q=collection%3A%22librivoxaudio%22&fl%5B%5D=downloads&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows=50&page=$i&callback=callback&save=yes&output=rss#raw" | awk -F'[<>]' '{ d[$2]=$3; if ($2=="/item" && index(d["description"],"BitTorrent") ) { print d["link"] } }' |parallel links -dump -html-numbered-links 1 | grep -o 'https://archive.org/.*torrent$' | sort -u)
+        torrent=$(cat /home/wings/upload-toolkit/fetcher/test.html | awk -F'[<>]' '{ d[$2]=$3; if ($2=="/item" && index(d["description"],"BitTorrent") ) { print d["link"] } }' |parallel links -dump -html-numbered-links 1 | grep -o 'https://archive.org/.*torrent$' | sort -u)
+#        torrent=$(wget -q -O- "https://archive.org/advancedsearch.php?q=collection%3A%22librivoxaudio%22&fl%5B%5D=downloads&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows=50&page=$i&callback=callback&save=yes&output=rss#raw" | awk -F'[<>]' '{ d[$2]=$3; if ($2=="/item" && index(d["description"],"BitTorrent") ) { print d["link"] } }' |parallel links -dump -html-numbered-links 1 | grep -o 'https://archive.org/.*torrent$' | sort -u)
 
         # Create a list of source URLs from the torrent URLs.
         sources=$(echo "$torrent" | awk 'BEGIN { FS = "/" } ; { print $5}')
@@ -53,10 +54,12 @@ do
 		IFS=
 		metadata=$(curl -s 'https://archive.org/metadata/'$sourceName'/metadata')
 		sourceTitle=$(echo $metadata | jq '.result.title + " by " + .result.creator')
-		export sourceDescription=$(echo $metadata | jq '.result.description' | html2text)
+		# Fetch the description of the content and throw it into a file.
+		curl -s 'https://archive.org/metadata/'$sourceName'/metadata/description' | jq '.result' | html2text -utf8 -nobs > description.tmp
                 # Run the special API upload script using our params
 		echo "Creating upload command in uploads.txt"
-                bash ~/upload-toolkit/fetcher/apiup-auto.sh "$torrentFilename" "https://archive.org/details/$sourceName" "$sourceTitle" >> uploads-"$timestamp".txt
+                python3 ~/upload-toolkit/fetcher/apiup-auto.py "$torrentFilename" "https://archive.org/details/$sourceName" "$sourceTitle"
+# >> uploads-"$timestamp".txt
 		echo >> uploads-"$timestamp".txt
 		unset IFS
         done
