6abc12f9bbb713bdea46121d63d1a1c98074529f
Benjamin Arntzen <zorlin@gmail.com>
2021-02-22 13:03:54 +0800

    add a terrible awful no-good script

---
+++ fetcher/create-torrents.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Get a list of releases
+folders=$(ls -1 .)
+
+# Turn it into an array
+arr=(`echo ${folders}`);
+
+# Now let's enter the loop.
+for i in "${arr[@]}"
+do
+        # Fetch the title of the release
+        metadata=$(curl -s 'https://archive.org/metadata/'$i'/metadata')
+        sourceTitle=$(echo $metadata | jq -r '.result.title + " by " + .result.creator')
+        echo $sourceTitle
+
+        # Create the release folder and hard link the files in
+        cd $i
+        mkdir "$sourceTitle"
+        ln * "$sourceTitle"/
+        # Cleanup unneeded files
+        rm "$sourceTitle"/*.png "$sourceTitle"/*.json "$sourceTitle"/*.xml "$sourceTitle"/*.sqlite "$sourceTitle"/*.m4b "$sourceTitle"/*.gz
+        rm "$sourceTitle"/*.txt "$sourceTitle"/*.zip "$sourceTitle"/*_itemimage.jpg "$sourceTitle"/*_thumb.jpg
+        # Remove lower quality mp3s
+        rm "$sourceTitle"/*_64kb.mp3
+        find "$sourceTitle"/ -type f -iname "*.mp3" | grep -v _128kb.mp3 | xargs -d "\n" -I {} rm \{}
+        ~/mktorrent.sh "$sourceTitle"/
+        cd ..
+done
