484dd87a97dedb7cb0db5e41bd37e9e44f0b9ad9
Benjamin Arntzen <zorlin@gmail.com>
2021-02-23 03:32:17 +0100

    Switch back to local files

--- fetcher/netwaves-gettorrents.sh
+++ fetcher/netwaves-gettorrents.sh
@@ -10,11 +10,12 @@ echo "Grabbing a list of torrent URLs."
 # When necessary, use a local snapshot to speed up the debug loop.
 # We use -q to silence wget,
 # and we output to stdout before parsing and passing to various utilities to handle the actual downloading.
-releaseNames=$(wget -O- "https://archive.org/advancedsearch.php?q=collection%3A%22netwaves%22&fl%5B%5D=downloads&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows=1000000000&callback=callback&save=yes&output=rss#raw" | awk -F'[<>]' '{ d[$2]=$3; if ($2=="/item" && index(d["description"],"BitTorrent") ) { print d["link"] } }')
-#releaseNames=$(cat ~/upload-toolkit/fetcher/netwaves.html | awk -F'[<>]' '{ d[$2]=$3; if ($2=="/item" && index(d["description"],"BitTorrent") ) { print d["link"] } }' | awk 'BEGIN { FS = "/" } ; { print $5}')
+#releaseNames=$(wget -O- "https://archive.org/advancedsearch.php?q=collection%3A%22netwaves%22&fl%5B%5D=downloads&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows=1000000000&callback=callback&save=yes&output=rss#raw" | awk -F'[<>]' '{ d[$2]=$3; if ($2=="/item" && index(d["description"],"BitTorrent") ) { print d["link"] } }')
+releaseNames=$(cat ~/upload-toolkit/fetcher/netwaves.html | awk -F'[<>]' '{ d[$2]=$3; if ($2=="/item" && index(d["description"],"BitTorrent") ) { print d["link"] } }' | awk 'BEGIN { FS = "/" } ; { print $5}')
 mapfile -t < <(echo $releaseNames)
 
 echo ${MAPFILE[@]}
+echo ${MAPFILE[1]}
 # Parse our list of releases and create a list of torrent URLs
 for releaseName in "${MAPFILE[@]}"
 do
