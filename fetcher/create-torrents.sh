c9020a63b09c4409477101867459e27ad7d2065a
Benjamin Arntzen <zorlin@gmail.com>
2021-03-01 03:32:13 +0100

    Skip torrents that did not work + other fixes

--- fetcher/create-torrents.sh
+++ fetcher/create-torrents.sh
@@ -29,33 +29,36 @@ do
 		echo "PANIK"
 		echo 1336
 	fi
+
 	echo "Grabbing the title for $releaseName"
         # Fetch the title of the release
         metadata=$(curl -s "https://archive.org/metadata/$releaseName/metadata")
-        sourceTitle=$(echo $metadata | jq -r '.result.title + " by " + .result.creator')
+        sourceTitle=$(echo "$metadata" | jq -r '.result.title + " by " + .result.creator')
         echo "Title: $sourceTitle"
 
 	if [ "$sourceTitle" == " by " ]; then
 		echo "sourceTitle is empty. This means something went wrong."
-		echo "PANIK"
-		exit 1337
+		echo "Skipping and logging the issue."
+		echo "Failed on $releaseName" >> ~/failures.log
 	fi
 
-        # Create the release folder and hard link the files in
-	echo "Creating folders."
-        mkdir -p "$sourceTitle" || { echo 'Failed to create folders! Exiting.' ; exit 1; }
-	echo "Hard linking."
-        ln "$releaseName/"* "$sourceTitle"/ 2>/dev/null
-        # Cleanup unneeded files
-	echo "Cleaning up unneeded files."
-        rm "$sourceTitle"/*.png "$sourceTitle"/*.m4b "$sourceTitle"/*.xml "$sourceTitle"/*.sqlite "$sourceTitle"/*.json "$sourceTitle"/*.gz 2>/dev/null
-        rm "$sourceTitle"/*.txt "$sourceTitle"/*.zip "$sourceTitle"/*.html "$sourceTitle"/*_itemimage.jpg "$sourceTitle"/*_thumb.jpg 2>/dev/null
-	rm "$sourceTitle"/*.ogg 2>/dev/null
-        # Remove lower quality mp3s
-	echo "Removing lower quality mp3s."
-        rm "$sourceTitle"/*_64kb.mp3 2>/dev/null
-        find "$sourceTitle"/ -type f -iname "*.mp3" | grep -v _128kb.mp3 | xargs -d "\n" -I {} rm \{} 2>/dev/null
-	echo "Building the torrent!"
-        ~/mktorrent.sh -n "$sourceTitle" -o "$releaseName.torrent" "$sourceTitle"/
-	#read -p "Press any key to keep going ..."
+	if [ "$sourceTitle" != " by " ]; then
+		# Create the release folder and hard link the files in
+		echo "Creating folders."
+		mkdir -p "$sourceTitle" || { echo 'Failed to create folders! Exiting.' ; exit 1; }
+		echo "Hard linking."
+		ln "$releaseName/"* "$sourceTitle"/ 2>/dev/null
+		# Cleanup unneeded files
+		echo "Cleaning up unneeded files."
+		rm "$sourceTitle"/*.png "$sourceTitle"/*.m4b "$sourceTitle"/*.xml "$sourceTitle"/*.sqlite "$sourceTitle"/*.json "$sourceTitle"/*.gz 2>/dev/null
+		rm "$sourceTitle"/*.txt "$sourceTitle"/*.zip "$sourceTitle"/*.html "$sourceTitle"/*_itemimage.jpg "$sourceTitle"/*_thumb.jpg 2>/dev/null
+		rm "$sourceTitle"/*.ogg 2>/dev/null
+		# Remove lower quality mp3s
+		echo "Removing lower quality mp3s."
+		rm "$sourceTitle"/*_64kb.mp3 2>/dev/null
+		find "$sourceTitle"/ -type f -iname "*.mp3" | grep -v _128kb.mp3 | xargs -d "\n" -I {} rm \{\} 2>/dev/null
+		echo "Building the torrent!"
+		~/mktorrent.sh -n "$sourceTitle" -o "$releaseName.torrent" "$sourceTitle"/
+		#read -p "Press any key to keep going ..."
+	fi
 done
