abd18833495e1be162e6e43aeb547c7e5e93e7e4
Benjamin Arntzen <zorlin@gmail.com>
2021-04-11 15:20:49 +0800

    Automatically trim titles when creating torrents + cleanup

--- uploadbot/create-torrents.sh
+++ uploadbot/create-torrents.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 # Enable debugging
 set -x
@@ -12,8 +11,8 @@ echo "Welcome to the Riff.CC toolkit! Let's have some fun."
 # You can filter for "*_librivox" for example if needed
 mapfile -t < <(find /mfsbrick.3/final1/ -maxdepth 1 -type d -not -wholename "/mfsbrick.3/final1/" -not -name "." -not -name ".." -printf '%P\n')
 
-echo "DEBUG - this is our list!"
-echo "${MAPFILE[@]}"
+#echo "DEBUG - this is our list!"
+#echo "${MAPFILE[@]}"
 
 # Create a counter
 counter=1
@@ -36,7 +35,11 @@ do
         # Fetch the title of the release
         metadata=$(curl -s "https://archive.org/metadata/$releaseName/metadata")
         sourceTitle=$(echo "$metadata" | jq '.result.title + " by " + (try (.result.creator | join(", ")) catch false // .result.creator)')
-	sourceTitleOnly=$(echo "$metadata" | jq '.result.title')
+	# Automatically trim the title if needed, to avoid issues with folder creation.
+	if [ "${#sourceTitle}" -ge 50 ]; then
+		sourceTitle="${sourceTitle:0:50}..."
+		echo $sourceTitle
+	fi
         echo "Title: $sourceTitle"
 
 	if [ "$sourceTitle" == " by " ]; then
@@ -54,20 +57,19 @@ do
 	if [ ! "$sourceTitle" == " by " ] && [ ! "$sourceTitle" == "" ]; then
 		# Create the release folder and hard link the files in
 		echo "Creating folders."
-		mkdir -p "/home/media/release/$sourceTitleOnly" || { echo 'Failed to create folders! Exiting.' ; exit 1; }
+		mkdir -p "/home/media/release/$sourceTitle" || { echo 'Failed to create folders! Exiting.' ; exit 1; }
 		echo "Hard linking."
-		cp -H "/mfsbrick.3/final1/$releaseName/"* "/home/media/release/$sourceTitleOnly"/ 2>/dev/null
+		cp -H "/mfsbrick.3/final1/$releaseName/"* "/home/media/release/$sourceTitle"/ 2>/dev/null
 		# Cleanup unneeded files
 		echo "Cleaning up unneeded files."
-		rm "/home/media/release/$sourceTitleOnly"/*.png "/home/media/release/$sourceTitleOnly"/*.m4b "/home/media/release/$sourceTitleOnly"/*.xml "/home/media/release/$sourceTitleOnly"/*.sqlite "/home/media/release/$sourceTitleOnly"/*.json "/home/media/release/$sourceTitleOnly"/*.gz 2>/dev/null
-		rm "/home/media/release/$sourceTitleOnly"/*.txt "/home/media/release/$sourceTitleOnly"/*.zip "/home/media/release/$sourceTitleOnly"/*.html "/home/media/release/$sourceTitleOnly"/*_itemimage.jpg "/home/media/release/$sourceTitleOnly"/*_thumb.jpg 2>/dev/null
-		rm "/home/media/release/$sourceTitleOnly"/*.ogg 2>/dev/null
+		rm "/home/media/release/$sourceTitle"/*.png "/home/media/release/$sourceTitle"/*.m4b "/home/media/release/$sourceTitle"/*.xml "/home/media/release/$sourceTitle"/*.sqlite "/home/media/release/$sourceTitle"/*.json "/home/media/release/$sourceTitle"/*.gz 2>/dev/null
+		rm "/home/media/release/$sourceTitle"/*.txt "/home/media/release/$sourceTitle"/*.zip "/home/media/release/$sourceTitle"/*.html "/home/media/release/$sourceTitle"/*_itemimage.jpg "/home/media/release/$sourceTitle"/*_thumb.jpg 2>/dev/null
+		rm "/home/media/release/$sourceTitle"/*.ogg 2>/dev/null
 		# Remove lower quality mp3s
 		echo "Removing lower quality mp3s."
-		rm "/home/media/release/$sourceTitleOnly"/*_64kb.mp3 2>/dev/null
-		find "/home/media/release/$sourceTitleOnly"/ -type f -iname "*.mp3" | grep -v _128kb.mp3 | xargs -d "\n" -I {} rm \{\} 2>/dev/null
+		rm "/home/media/release/$sourceTitle"/*_64kb.mp3 2>/dev/null
+		find "/home/media/release/$sourceTitle"/ -type f -iname "*.mp3" | grep -v _128kb.mp3 | xargs -d "\n" -I {} rm \{\} 2>/dev/null
 		echo "Building the torrent!"
-		/home/media/mktorrent.sh -n "$sourceTitle" -o "torrents/$releaseName.torrent" "/home/media/release/$sourceTitleOnly"/
-		#read -p "Press any key to keep going ..."
+		/home/media/mktorrent.sh -n "$sourceTitle" -o "torrents/$releaseName.torrent" "/home/media/release/$sourceTitle"
 	fi
 done
