d6ee9c2c5be46b20c9f3117fe5cb2d800f3cc862
Benjamin Arntzen <zorlin@gmail.com>
2021-04-25 14:43:47 +0200

    Implement proper logging for cleanup

--- uploadbot/create/rcc_cleanup.py
+++ uploadbot/create/rcc_cleanup.py
@@ -7,10 +7,23 @@
 import os
 import sys
 import yaml
+import datetime
+import logging
 from internetarchive import get_item
 
 # Settings
 maxlength = 250 # max length a name can be before it is truncated
+
+# Set up logging
+# TODO datestamp
+logger = logging.getLogger('loggerLogger')
+
+import datetime
+now = datetime.datetime.now()
+
+timestamp = now.strftime('%Y-%m-%d-T-%H.%M.%S')
+handler = logging.FileHandler('logs/hardlinks-' + timestamp + '.log')
+logger.addHandler(handler)
 
 # Set the source directory (fromPath) and the place with our releases (toPath)
 fromPath="/mfsbrick.3/final1"
@@ -20,6 +33,7 @@ print(dirs)
 
 for download in dirs:
 	print("Working on: " + download)
+	logger.info("Working on: "+ download)
 
 	# Fetch metadata from archive.org for the item, then pull out data
 	item = get_item(download)
@@ -39,7 +53,8 @@ for download in dirs:
 		creator = item.item_metadata['metadata']['creator']
                 # Check if we have a list, and convert it to a string if needed.
 		if type(creator) == list:
-			print("join")
+			print("Converting creator/author list to string")
+			logger.info("Converting creator/author list to string")
 			creator = ", ".join(creator)
 	except:
 		print("Unknown creator or error fetching creator for "+ download)
@@ -54,15 +69,12 @@ for download in dirs:
 	# Dynamically load in our magic config files
 	config = yaml.safe_load(open("rules/" + collection + ".yml"))
 
-	print(config)
-
 	# Check if the config is empty
 	if config is None:
 		print("Failed to load config file for collection: " + collection)
 		sys.exit(1338)
 
 	if not config["extensions"] == "this_page_intentionally_left_blank":
-		print("extensions!")
 		for root,dirs,files in os.walk(toPath + "/" + targetdirname):
 			for file in files:
 				testFile = os.path.join(root,file)
@@ -70,10 +82,10 @@ for download in dirs:
 
 				if(testFile.endswith(tuple(config["extensions"]))):
 					print("Removing (failed extension): " +testFile)
+					logger.info("Removing (failed extension): " +testFile)
 					os.unlink(testFile)
 
 	if not config["qualities"] == "this_page_intentionally_left_blank":
-		print("qualities!")
 		for root,dirs,files in os.walk(toPath + "/" + targetdirname):
 			for file in files:
 				testFile = os.path.join(root,file)
@@ -81,4 +93,5 @@ for download in dirs:
 
 				if(testFile.endswith(tuple(config["qualities"]))):
 					print("Removing (failed quality): " +testFile)
+					logger.info("Removing (failed quality): " +testFile)
 					os.unlink(testFile)
